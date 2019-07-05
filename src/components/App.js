2cffe56555c87d0aa03feda60dd99a59c6b304e1
jinfull <jinfull@gmail.com>
2019-07-05 14:27:05 -0700

    Update app.js to contain login in switch statement, update header to include login link

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import LinkList from './LinkList';
 import CreateLink from './CreateLink';
 import Header from './Header';
+import Login from './Login';
 import { Switch, Route } from 'react-router-dom';
 
 
@@ -14,6 +15,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" component={LinkList} />
             <Route exact path="/create" component={CreateLink} />
+            <Route exact path="/login" component={Login} />
           </Switch>
         </div>
       </div>
